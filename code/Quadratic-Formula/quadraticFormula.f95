1341dadcf5e1f10e729a0f63bb3ca3bafc75882e
Joseph-Chierichella <josephchierichella53@gmail.com>
2025-06-28 00:56:36 -0400

    Create quadraticFormula.f95

---
+++ code/Quadratic-Formula/quadraticFormula.f95
@@ -0,0 +1,21 @@
+
+!In order to compile this you must have gfortran
+
+! Your input should be in this format -->  a b c
+! For example, you input could be     -->  1 -3 2
+
+PROGRAM quadraticFormula
+        IMPLICIT NONE
+        DOUBLE PRECISION :: a, b, c
+        DOUBLE PRECISION :: root1, root2
+
+        PRINT *, "Enter coefficients a, b, and c:"
+        READ *, a, b, c
+
+        root1 = (-b + SQRT(b**2 - 4.0d0*a*c)) / (2.0d0*a)
+        root2 = (-b - SQRT(b**2 - 4.0d0*a*c)) / (2.0d0*a)
+
+        PRINT *, "Root 1 =" , root1
+        PRINT *, "Root 2 =" , root2
+END PROGRAM quadraticFormula
+
